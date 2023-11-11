f8c2de9d3c06a8caecf4d335d59a4b22e81b998a
mohi14 <mohiuddinkhan1409@gmail.com>
2023-11-11 10:21:45 +0600

    user api done

---
+++ modules/user/user.route.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  registerUser,
+  emailVirification,
+  loginUser,
+  getAllUsers,
+  deleteUser,
+  getUser,
+  editUser,
+  uploadDocuments,
+} = require("./user.controller");
+
+const router = express.Router();
+
+router.post("/signup", registerUser);
+router.post("/login", loginUser);
+router.get("/", getAllUsers);
+router.delete("/delete/:id", deleteUser);
+router.put("/upload/:id", uploadDocuments);
+router.put("/edit/:id", editUser);
+router.get("/:id", getUser);
+router.get("/verify/:email", emailVirification);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 8000;
 
+const usersRoutes = require("./modules/user/user.route");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/users", usersRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is runnig");
