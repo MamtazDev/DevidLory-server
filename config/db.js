b45a7cc7f8c1b6c7443c52725a1758d035abb3d3
mohi14 <mohiuddinkhan1409@gmail.com>
2023-11-11 10:42:58 +0600

    mongodb connection issue solved

--- config/db.js
+++ config/db.js
@@ -4,11 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.6ds5s8q.mongodb.net/davidlory`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.6ds5s8q.mongodb.net/davidlory`
     );
     console.log("Connected to the mongodb database successfully!");
   } catch (error) {
