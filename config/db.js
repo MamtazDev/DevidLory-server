b808fb1155b059a81db648d41659f89eeddc3b54
mohi14 <mohiuddinkhan1409@gmail.com>
2023-11-11 10:10:29 +0600

    project init

---
+++ config/db.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.6ds5s8q.mongodb.net/davidlory`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Connected to the mongodb database successfully!");
+  } catch (error) {
+    console.log("mongodb connection failed", error.message);
+  }
+};
+
+module.exports = connectDB;
