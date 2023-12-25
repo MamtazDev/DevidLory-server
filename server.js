6acaa3a0b8f598cf148e21812d394cd4ba33c1ae
delower-hasan <dhhasansaha11@gmail.com>
2023-12-25 23:01:39 +0600

    little update

--- server.js
+++ server.js
@@ -63,50 +63,50 @@ const upload = multer({
   },
 });
 
-app.get("/api/get-file-path", async (req, res) => {
-  let files = await allAudios();
-  let audioFiles = JSON.parse(files);
-  res.json(audioFiles);
-});
+// app.get("/api/get-file-path", async (req, res) => {
+//   let files = await allAudios();
+//   let audioFiles = JSON.parse(files);
+//   res.json(audioFiles);
+// });
 
 // audio streming site
-app.get("/api/audio", async (req, res) => {
-  // get audio file
-  const { filename } = req.query;
-  let files = await AudiofilePath(filename);
-  let audioFileName = JSON.parse(files);
+// app.get("/api/audio", async (req, res) => {
+//   // get audio file
+//   const { filename } = req.query;
+//   let files = await AudiofilePath(filename);
+//   let audioFileName = JSON.parse(files);
 
-  const filePath = path.join(
-    __dirname,
-    `/assets/audio-files/${audioFileName}.mp3`
-  ); // Replace with your audio file path
-  const stat = fs.statSync(filePath);
-  const fileSize = stat.size;
-  const range = req.headers.range;
+//   const filePath = path.join(
+//     __dirname,
+//     `/assets/audio-files/${audioFileName}.mp3`
+//   ); // Replace with your audio file path
+//   const stat = fs.statSync(filePath);
+//   const fileSize = stat.size;
+//   const range = req.headers.range;
 
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-    const chunksize = end - start + 1;
-    const file = fs.createReadStream(filePath, { start, end });
-    const head = {
-      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": chunksize,
-      "Content-Type": "audio/mp3",
-    };
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      "Content-Length": fileSize,
-      "Content-Type": "audio/mp3",
-    };
-    res.writeHead(200, head);
-    fs.createReadStream(filePath).pipe(res);
-  }
-});
+//   if (range) {
+//     const parts = range.replace(/bytes=/, "").split("-");
+//     const start = parseInt(parts[0], 10);
+//     const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+//     const chunksize = end - start + 1;
+//     const file = fs.createReadStream(filePath, { start, end });
+//     const head = {
+//       "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+//       "Accept-Ranges": "bytes",
+//       "Content-Length": chunksize,
+//       "Content-Type": "audio/mp3",
+//     };
+//     res.writeHead(206, head);
+//     file.pipe(res);
+//   } else {
+//     const head = {
+//       "Content-Length": fileSize,
+//       "Content-Type": "audio/mp3",
+//     };
+//     res.writeHead(200, head);
+//     fs.createReadStream(filePath).pipe(res);
+//   }
+// });
 
 app.put("/upload/:id", upload.single("file"), async (req, res) => {
   console.log("req", req.file);
